705804525b3596f434dd2f5c27ff3ed0d400ceae
abhixxjeet <abhijeetwork8311@gmail.com>
2026-02-05 19:10:59 +0530

    Time: 7 ms (85.16%) | Memory: 10.4 MB (78.78%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n=coins.size();
+        vector<long long>dp(amount+1,0);
+        dp[0]=1;
+        for(int i=0;i<n;i++){
+           for(int j=coins[i];j<=amount;j++){
+            dp[j]+=dp[j-coins[i]];
+            if(dp[j]>INT_MAX){
+                dp[j]=INT_MAX;
+            }
+           }
+        }
+        return (int)dp[amount];
+    }
+};
