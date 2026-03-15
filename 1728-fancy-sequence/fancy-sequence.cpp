73db21472c907fc266734524f48b76fde917de89
abhixxjeet <abhijeetwork8311@gmail.com>
2026-03-15 09:17:02 +0530

    Time: 64 ms (80.29%) | Memory: 192.3 MB (85.40%) - LeetSync

---
+++ 1728-fancy-sequence/fancy-sequence.cpp
@@ -0,0 +1,54 @@
+class Fancy {
+public:
+    
+    const long long MOD=1e9+7;
+    vector<long long>seq;
+    long long mul=1;
+    long long add=0;
+
+    long long modInverse(long long x){
+        long res=1;
+        long long p=MOD-2;
+        while(p){
+            if(p&1){
+                res=(res*x)%MOD;
+            }
+            x=(x*x)%MOD;
+            p>>=1;
+            
+        }
+        return res;
+    }
+    Fancy() {
+        
+    }
+    
+    void append(int val) {
+        long long v=(val-add+MOD)%MOD;
+        v=(v* modInverse(mul))%MOD;
+        seq.push_back(v);
+    }
+    
+    void addAll(int inc) {
+        add=(add+inc)%MOD;
+    }
+    
+    void multAll(int m) {
+        mul=(mul*m)%MOD;
+        add=(add*m)%MOD;
+    }
+    
+    int getIndex(int idx) {
+        if(idx>=seq.size()) return -1;
+        return (seq[idx]*mul+add)%MOD;
+    }
+};
+
+/**
+ * Your Fancy object will be instantiated and called as such:
+ * Fancy* obj = new Fancy();
+ * obj->append(val);
+ * obj->addAll(inc);
+ * obj->multAll(m);
+ * int param_4 = obj->getIndex(idx);
+ */
