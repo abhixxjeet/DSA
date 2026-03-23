124c7b61b3e498e3327b9360476732cf2ea50113
abhixxjeet <abhijeetwork8311@gmail.com>
2026-03-24 00:21:22 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (84.82%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-       int n=ratings.size();
+        int n=ratings.size();
+        int i=1;
+        int candy=n;
 
-       vector<int>L2R(n,1);
-       vector<int>R2L(n,1);
+        while(i<n){
 
-       int result=0;
-
-       for(int i=1;i<n;i++){
-        if(ratings[i]>ratings[i-1]){
-        L2R[i]=max(L2R[i],L2R[i-1]+1);
-        }
-       }
-
-       for(int i=n-2;i>=0;i--){
-          if(ratings[i]>ratings[i+1]){
-            R2L[i]=max(R2L[i],R2L[i+1]+1);
+            if(ratings[i]==ratings[i-1]){
+                i++;
+                continue;
             }
-       }
+            int peak=0;
+            while(ratings[i]>ratings[i-1]){
+                peak++;
+                candy+=peak;
+                i++;
+                if(i==n){
+                    return candy;
+                }
+            }
 
-       for(int i=0;i<n;i++){
-         result+=max(R2L[i],L2R[i]);
-       }
+            int dip=0;
+            while(i<n && ratings[i]<ratings[i-1]){
+                dip++;
+                candy+=dip;
+                i++;
+            }
 
-       return result;
+            candy-=min(peak,dip);
+        }
 
+        return candy;
     }
 };
