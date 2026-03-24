ee526660ce249f066262c709bfd556a9a9730cb4
abhixxjeet <abhijeetwork8311@gmail.com>
2026-03-24 20:59:51 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (17.05%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -6,16 +6,16 @@ public:
         vector<int>rg(n);
         lg[0]=height[0];
         for(int i=1;i<n;i++){
-          lg[i]=max(lg[i-1],height[i]);
+           lg[i]=max(lg[i-1],height[i]);
         }
         rg[n-1]=height[n-1];
         for(int i=n-2;i>=0;i--){
             rg[i]=max(rg[i+1],height[i]);
         }
-        int result=0;
+        int total=0;
         for(int i=1;i<n-1;i++){
-           result+=min(lg[i],rg[i])-height[i];
+            total+=min(rg[i],lg[i])-height[i];
         }
-        return result;
+        return total;
     }
 };
