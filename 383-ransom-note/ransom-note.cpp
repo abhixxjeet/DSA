d581f0ab31dcf55131a3e6dbe929d542844fbf83
abhixxjeet <abhijeetwork8311@gmail.com>
2026-04-25 10:40:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (48.38%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int>mp;
+
+        for(char c : magazine){
+            mp[c]++;
+        }
+
+        for(char c : ransomNote){
+           if(mp[c]==0){
+            return false;
+           }
+           else{
+            mp[c]--;
+           }
+        }
+        return true;
+    }
+};
