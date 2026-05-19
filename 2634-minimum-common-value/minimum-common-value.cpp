f6375157eb55e269d85ed58d35d54a6d726717a4
abhixxjeet <abhijeetwork8311@gmail.com>
2026-05-19 11:42:30 +0530

    Time: 0 ms (100.00%) | Memory: 54.6 MB (58.81%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
 
         int n=nums1.size();
         int m=nums2.size();
