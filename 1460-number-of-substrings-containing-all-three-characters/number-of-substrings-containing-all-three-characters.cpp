fdd9192911a37bf26ba39fbf1660a606377820c1
abhixxjeet <abhijeetwork8311@gmail.com>
2026-06-30 22:38:44 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (57.43%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n=s.length();
-        unordered_map<char,int>mp;
-        int j=0;
-        int i=0;
+        int last_a=-1,last_b=-1,last_c=-1;
+
         int count=0;
-        int result=0;
-        while(i<n){
-            mp[s[i]]++;
-            while(mp['a'] >=1 && mp['b']>=1 && mp['c']>=1){
-                
-                result+=n-i;
-                mp[s[j]]--;
-                j++;
+
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='a'){
+                last_a=i;
             }
-            i++;
+            else if(s[i]=='b'){
+                last_b=i;
+            }
+            else{
+                last_c=i;
+            }
+
+
+            int min_index=min(last_a,min(last_b,last_c));
+
+
+            if(min_index!=-1){
+                count+=(min_index+1);
+            }
         }
-        return result;
+
+
+        return count;
     }
 };
